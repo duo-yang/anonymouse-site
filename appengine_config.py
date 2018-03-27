669677b1a12d5227f4817f1965c66515005c365b
duo-yang <yangduots@163.com>
2018-03-27 02:27:23 -0400

    Add code

--- appengine_config.py
+++ appengine_config.py
@@ -4,3 +4,8 @@ import os.path
 # add `lib` subdirectory to `sys.path`, so our `main` module can load
 # third-party libraries.
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
+
+on_appengine = os.environ.get('SERVER_SOFTWARE', '').startswith('Development')
+
+if on_appengine and os.name == 'nt':
+    sys.platform = "Not Windows"
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
