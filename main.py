f5e778cbcc38c588bbc8e546ce56865e2eb4c2f2
amorymanuel <amory@umich.edu>
2018-12-09 14:24:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/about/')
+@app.route('/about/', methods=['GET', 'POST'])
 def about():
     return render_template('aboutpage.html')
 
