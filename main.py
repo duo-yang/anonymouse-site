4f6e3a966c411ae926fe8fcc3b8665d0d96d5e95
Brian Lee <blee025432@gmail.com>
2019-05-15 13:58:30 -0400

    converted back to tabs and implemented cli mode

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/about/', methods=['GET', 'POST'])
 def about():
-    return render_template('aboutpage.html')
+	return render_template('aboutpage.html')
 
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
