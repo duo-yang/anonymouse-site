f8ace6031974817d6aeebd6626bfa54839658b75
Robert Dick <dickrp@umich.edu>
2019-06-26 10:40:35 -0400

    Automated script.

--- exec.sh
+++ exec.sh
@@ -1,4 +1,18 @@
+#!/bin/sh
+
+killall flask
+
+if [ ! -e env ]; then
+	python -m venv env
+fi
+
+if [ -z ${BROWSER+x} ]; then
+	BROWSER="firefox"
+fi
+
 source env/bin/activate
 export FLASK_APP=main
 export FLASK_ENV=development
-flask run
+flask run &
+sleep 3
+${BROWSER} http://127.0.0.1:5000/
