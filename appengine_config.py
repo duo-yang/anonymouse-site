d6ced7dfaff6958e4f27ecf922fbc73dc69f6ffe
Duo Yang <yangduo1994@gmail.com>
2022-01-30 04:25:24 -0800

    Include third-party packages

--- appengine_config.py
+++ appengine_config.py
@@ -1,6 +1,7 @@
 """`appengine_config` gets loaded when starting a new application instance."""
 import sys
 import os.path
+from google.appengine.ext import vendor
 # add `lib` subdirectory to `sys.path`, so our `main` module can load
 # third-party libraries.
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
@@ -9,3 +10,6 @@ on_appengine = os.environ.get('SERVER_SOFTWARE', '').startswith('Development')
 
 if on_appengine and os.name == 'nt':
     sys.platform = "Not Windows"
+
+# Add any libraries install in the "lib" folder.
+vendor.add('lib')
